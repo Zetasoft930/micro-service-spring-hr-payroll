e0b14625a6149bd5d0d2bc6d3f97b576a2e54976
zetasoft930 <celsomateussousa@gmail.com>
2023-11-06 14:51:46 +0100

    primeiro commit

---
+++ src/test/java/ao/co/celsodesousa/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.co.celsodesousa.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
